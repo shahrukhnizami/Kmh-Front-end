d9d002e8db989587bdf17b14ee4ad3c67f38cf02
shahrukhnizami <nizamishahrukh12@gmail.com>
2025-04-19 15:33:51 +0500

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+  
+  
+// })
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  define: {
-    'process.env': process.env
-  }
-  
-})
+});
